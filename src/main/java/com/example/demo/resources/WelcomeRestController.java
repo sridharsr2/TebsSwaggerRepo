32d06834240e7049f6ff2214cee0fe2c56feca88
sridharsr2 <36062905+sridharsr2@users.noreply.github.com>
2025-12-18 15:18:09 +0800

    Update WelcomeRestController.java

--- src/main/java/com/example/demo/resources/WelcomeRestController.java
+++ src/main/java/com/example/demo/resources/WelcomeRestController.java
@@ -14,7 +14,7 @@ public class WelcomeRestController {
 	@ApiOperation(value = "Get Welcome Message For The Given Name ", response = String.class, tags = "getWelcomeNote")
 	@GetMapping("/welcome/{name}")
 	public String welcome(@PathVariable("name") String name) {
-		return name+", Welcome to Tebs ..!!";
+		return name+", Welcome to Tebsss ..!!";
 	}
 
 }
